fa22ae3439e4b135bc17966d596a989730dcfade
hidelrios <hidels.rios@gmail.com>
2026-06-01 21:44:50 -0300

    fix: corrige hover do botão

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,52 @@
-export const Button = () => {
-    
+import { cva, type VariantProps } from "class-variance-authority";
+import Text from "./text";
+import type { ComponentProps } from "react";
+
+export const buttonVariants = cva(
+  `flex items-center justify-center`,
+  {
+    variants: {
+      variant: {
+        primary: "bg-yellow border-2 border-transparent rounded-lg transition-colors hover:border-yellow-light",
+      },
+      size: {
+        md: "w-full h-14",
+      },
+      disabled: {
+        true: "bg-yellow-dark opacity-50 hover:pointer-events-none",
+      },
+      handling: {
+        true: "cursor-pointer",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+      disabled: false,
+      handling: false,
+    }
+  }
+
+)
+interface ButtonProps
+  extends Omit<ComponentProps<"button">, "size" | "disabled">,
+  VariantProps<typeof buttonVariants> {
+  handling?: boolean;
+}
+export default function Button({
+  variant,
+  size,
+  className,
+  disabled,
+  handling,
+  children,
+  ...props }: ButtonProps) {
+  return (
+    <button className={buttonVariants({ className, size, variant, disabled, handling })}
+      {...props}>
+      <Text variant="title-sm" className="text-gray-900">
+        {children}
+      </Text>
+    </button>
+  )
 }
--- src/core/components/main-content.tsx
+++ src/core/components/main-content.tsx
@@ -1,7 +1,12 @@
+import Button from "../../components/button";
+
 export default function MainContent() {
   return (
     <div className="bg-gray-800 text-gray-100">
       <h1>Main Page</h1>
+      <Button handling={true}>AGENDAR</Button>
+      <Button disabled={true}>AGENDAR</Button>
+
     </div>
   );
 }
