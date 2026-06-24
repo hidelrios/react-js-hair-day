7037c32b93d76ba6bc337922eaee0a60be2054c6
hidelrios <hidels.rios@gmail.com>
2026-06-23 22:51:18 -0300

    ajusta design do input de data

--- src/components/date-input.tsx
+++ src/components/date-input.tsx
@@ -1,14 +1,15 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import React from "react";
+import React, { useRef } from "react";
 import Icon from "./icon";
 import Calendar from "../assets/icons/calendar-blank.svg?react";
 import CaretDown from "../assets/icons/caret-down.svg?react";
 
 const dateInputWrapperVariants = cva(`
-  rounded-lg border border-solid border-gray-600 bg-gray-800 inline-flex items-center w-full h-14 px-4 gap-3`
+  cursor-pointer rounded-lg border border-solid border-gray-500 bg-gray-700 flex items-center justify-center w-full h-14 px-4 gap-3`
 )
 
 const dateInputVariants = cva(`
+  cursor-pointer outline-none bg-transparent text-gray-200 text-sm w-full
     [&::-webkit-calendar-picker-indicator]:hidden
   `
 )
@@ -18,11 +19,15 @@ interface DateInputProps extends VariantProps<typeof dateInputVariants>,
 
 }
 export default function DateInput({ className, ...props }: DateInputProps) {
+
   return (
     <div className={dateInputWrapperVariants({ className })}>
-      <Icon svg={Calendar} className="fill-yellow" />
-      <input type="date" className={dateInputVariants({ className })} {...props} />
-      <Icon svg={CaretDown} className="fill-gray-400" />
+      <div className="flex items-center w-full gap-3">
+        <Icon svg={Calendar} className="fill-yellow size-5" />
+        <input type="date" className={dateInputVariants({ className })} {...props} />
+        <Icon svg={CaretDown} className="fill-gray-400 size-5 ml-auto" />
+      </div>
+
     </div>
   )
 }
